9ab36b970a44e5f23d61f56dbde2bd9d21dce77f
Hristo Isov <78639110+Hristo-Isov@users.noreply.github.com>
2024-01-21 14:29:38 +0200

    added a bool function that checks if 2 matrixes are equal

--- Nonogram_Mater_Project/AllFunctions.h
+++ Nonogram_Mater_Project/AllFunctions.h
@@ -1,1 +1,36 @@
 #pragma once
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+const int MAX_SIZE = 20;
+const int MAX_WRONG_ANSWERS = 3;
+const int MAX_DIFFICULTY_LEVEL = 5;
+
+struct UserProfile {
+    string username;
+    int currentLevel;
+    int currentWrongAnswers;
+    char currentGrid[MAX_SIZE][MAX_SIZE];
+};
+
+struct NonogramLevel {
+    int size;
+    int maxWrongAnswers;
+    char picture[MAX_SIZE][MAX_SIZE];
+};
+
+bool areMatricesEqual(char grid[MAX_SIZE][MAX_SIZE], char picture[MAX_SIZE][MAX_SIZE], int size) {
+    for (size_t i = 0; i < size; ++i) {
+        for (size_t j = 0; j < size; ++j) {
+            if (grid[i][j] != picture[i][j]) {
+                return false;
+            }
+        }
+    }
+    cout << "Matrices are equal\n";
+    return true;
+}
